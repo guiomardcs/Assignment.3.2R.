b62aa87ba94f83244d70207ac50d77687ebb1cbf
guiomardcs <157806160+guiomardcs@users.noreply.github.com>
2024-01-26 14:08:57 +0100

    Add files via upload

---
+++ remind_me_function.R
@@ -0,0 +1,10 @@
+my_list <- list()
+remind_me <- function(quantity = NA, item = NA) {
+  if (is.na(quantity) && is.na(item) == TRUE){
+    print(my_list)
+  } else {
+    item <- as.character(substitute(item))
+    items <- paste(quantity, item, sep = " x ")
+    my_list <<- append(my_list, items)
+  }
+}
