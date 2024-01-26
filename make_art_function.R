cb85b0cd0ec88ced89ac6774c4acba8039fef486
guiomardcs <157806160+guiomardcs@users.noreply.github.com>
2024-01-26 14:12:44 +0100

    Add files via upload

---
+++ make_art_function.R
@@ -0,0 +1,16 @@
+library(circlize)
+
+make_art <- function(seed = NULL){
+  if(!is.null(seed)){
+    seed <- set.seed()
+  }
+  plot(1:10, axes = FALSE, ann = FALSE, type = "n")
+  for(i in 1:10){
+    segments(runif(1, min = 1, max = 10), runif(1, min = 1, max = 10), 
+             runif(1, min = 1, max = 10), runif(1, min = 1, max = 10), 
+             col = rand_color(1))
+  }
+  for(i in 1:10){
+    lines(runif(10, min = 1, max = 10), col = rand_color(1))
+  }
+}
