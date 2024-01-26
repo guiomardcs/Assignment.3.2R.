40f6e958638d14c4b8ead6f69b178b89fa342329
guiomardcs <157806160+guiomardcs@users.noreply.github.com>
2024-01-26 14:10:18 +0100

    remind_me_function.R

--- remind_me_function.R
+++ remind_me_function.R
@@ -1,8 +1,9 @@
 my_list <- list()
 remind_me <- function(quantity = NA, item = NA) {
-  if (is.na(quantity) && is.na(item) == TRUE){
+  if (is.na(quantity) && is.na(item) == TRUE) {
     print(my_list)
-  } else {
+  }
+  else {
     item <- as.character(substitute(item))
     items <- paste(quantity, item, sep = " x ")
     my_list <<- append(my_list, items)
